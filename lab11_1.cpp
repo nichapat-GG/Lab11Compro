96c46150bc74c26744cf110ebaf59b0f701e816b
Nichapat Kulfu <nichapat.farsai@gmail.com>
2025-12-26 16:10:37 +0700

    Update lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,15 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<string>
+using namespace std;
+
+int main(){
+    srand(time(0));
+    string grade[] = {"A","B+","B","C+","C","D+","D","F","W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    int randomgrade = rand()%9;
+    cout << "You will get "<< grade[randomgrade] <<" in this 261102.";
+}
